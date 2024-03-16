874e5d5c74918fa4bbddd6dd4ced7085cca572e4
Yashwant Das <yashworks@gmail.com>
2024-03-16 05:51:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.dbConfig = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "mysql-166246-0.cloudclusters.net",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PASSWORD || "",
+  port: process.env.DB_PORT || 19753,
+  database: process.env.DB_NAME || "UsersDB",
 };
